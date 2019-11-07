309332c081703f4e984ff2833c493ff959346c99
Eve Stockstill <evestockstillpdx@gmail.com>
2019-11-06 20:11:09 -0800

    setUptree

---
+++ Public/animal-form/animal-form.js
@@ -0,0 +1,4 @@
+// import App from './ResourceFormApp.js';
+
+const app = new App();
+document.body.prepend(app.renderDOM());
---
+++ Public/animal-list/animal-list.js
@@ -0,0 +1,4 @@
+// import App from './ResourceApp.js';
+
+const app = new App();
+document.body.prepend(app.renderDOM());
