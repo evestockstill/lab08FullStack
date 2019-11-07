e850d955a57bb4085837ab9487addb3c58269b0a
Eve Stockstill <evestockstillpdx@gmail.com>
2019-11-07 15:29:40 -0800

    changed to type

--- data/monkeyObject.js
+++ data/monkeyObject.js
@@ -5,7 +5,7 @@ module.exports = [
         old_world: false,
         new_world: true,
         weight: '1 pound',
-        family: 'Callitrichidae',
+        type: 'Callitrichidae',
         summary: 'Gregarious and playful in the wild. Communication is a key behavior, coordinating grouos of Emperor Monkeys. Distinctive long mustache. Lives mostly in the Amazonian lowlands and Lowermountain Rainforest. Consumes mostly fruits, veggies and flowers. They have a high likelihood of having a twin sibling.'
 
 
