8dcc6573ce5aea7c670abbb6f29a54ae7844b46c
Eve Stockstill <evestockstillpdx@gmail.com>
2019-11-07 10:32:14 -0800

    added some components

--- Public/common/Header.js
+++ Public/common/Header.js
@@ -2,7 +2,19 @@ import Component from '../Component.js';
 
 class Header extends Component {
     renderHTML() {
+        const title = this.props.title || 'Monkeys';
 
+        return /*html*/`
+            <header>
+                
+                <h1>${title}</h1>
+                <nav>
+                    <a href="./">Home</a>
+                    <a href="./monkey-list.html">Monkeys</a>
+                    <a href="./monkey-form.html">Add a Monkey</a>
+                </nav>
+            </header>
+        `;
     }
 }
 
