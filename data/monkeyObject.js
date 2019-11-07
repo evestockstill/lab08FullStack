501b6b420c8876f270feaae700d067e64dc972b2
Eve Stockstill <evestockstillpdx@gmail.com>
2019-11-07 14:13:33 -0800

    added family

--- data/monkeyObject.js
+++ data/monkeyObject.js
@@ -1,11 +1,11 @@
 module.exports = [
     {
         name: 'Emporer Tamarin Monkey',
-        image_url: './Public/assets/emporer.jpg',
+        image: './Public/assets/emporer.jpg',
         old_world: false,
         new_world: true,
-        common_weight: '1 ',
-        common_height: '10 ',
+        weight: '1 pound',
+        family: 'Callitrichidae',
         summary: 'Gregarious and playful in the wild. Communication is a key behavior, coordinating grouos of Emperor Monkeys. Distinctive long mustache. Lives mostly in the Amazonian lowlands and Lowermountain Rainforest. Consumes mostly fruits, veggies and flowers. They have a high likelihood of having a twin sibling.'
 
 
