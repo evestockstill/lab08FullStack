a818e3d69bb4dee41e6628634c7d9d8593e69279
Eve Stockstill <evestockstillpdx@gmail.com>
2019-11-08 15:39:59 -0800

    fix

--- data/drop-tables.js
+++ data/drop-tables.js
@@ -11,7 +11,7 @@ async function run() {
         await client.connect();
 
         await client.query(`
-            DROP TABLE IF EXISTS monkey;
+            DROP TABLE IF EXISTS monkeys;
             DROP TABLE IF EXISTS types;
         `);
 
