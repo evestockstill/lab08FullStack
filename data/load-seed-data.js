1c696d8ada5ded158dea1bd9f374e542e7149f2e
Nick <nickbrimmer@gmail.com>
2019-11-08 10:47:21 -0800

    getting the load-seed-data completed

--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -13,20 +13,46 @@ async function run() {
         await client.connect();
 
         // "Promise all" does a parallel execution of async tasks
-        await Promise.all(
+        const savedTypes = await Promise.all(
             // map every item in the array data
-            monkeys.map(monkey => {
-                
+            monkeys.map(async monkey => {
+                const result = await client.query(`
+                    INSERT INTO monkeys (name)
+                    VALUES ($1)
+                    RETURNING *;
+                    
+                    `,
+                    
+                [monkey]);
 
-                return client.query(`
-                    INSERT INTO monkeys (name, image, old_world, new_world, weight, type, summary)
-                    VALUES ($1, $2, $3, $4, $5, $6, $7)
-                `, 
-                
-                [monkey.name, monkey.image, monkey.old_world, monkey.new_world, monkey.weight, monkey.type, monkey.summary]);
+                return result.rows[0];
 
             })
         );
+        [
+            { name: 'Atelidae', id: 1 },
+            { name: 'Callitrichidae', id: 2 },
+            { name: 'Cebidae', id: 3 },
+            { name: 'Cercopithecidae', id: 4 },
+              
+
+        ];
+            
+        await Promise.all(
+            monkeys.map(monkey => {
+
+                const type = savedTypes.find(type => {
+                    return type.name === monkey.type;
+                });
+                return client.query(`
+                        INSERT INTO monkeys (name, image, old_world, new_world, weight, type, summary)
+                        VALUES ($1, $2, $3, $4, $5, $6, $7)
+                    `, 
+                [monkey.name, monkey.image, monkey.old_world, monkey.new_world, monkey.weight, monkey.type, monkey.summary]);
+            })
+        );
+
+                
 
         console.log('seed data load complete');
     }
@@ -36,5 +62,6 @@ async function run() {
     finally {
         client.end();
     }
-
 }
+
+
